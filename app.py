e8dfa3e09ad76af740da5c7394c253f07e5e2cda
Anillab <anillabusolo@gmail.com>
2020-02-19 09:44:14 +0000

    defined the bot function

--- app.py
+++ app.py
@@ -5,3 +5,24 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app=Flask(__name__)
 
+@app.route('/bot',methods=['POST'])
+def bot():
+    incoming_msg=request.values.get('Body','').lower()
+    resp=MessagingResponse()
+    msg=resp.message()
+    responded=False
+    if 'quote' in incoming_msg:
+        r=requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data=r.json()
+            quote=f'{data["content"]}({data["author"]})'
+        else:
+            quote='I coould rretrieve a quote'
+        msg.body(quote)
+        responded=True
+    if 'cat' in incoming_msg:
+        msg.media('https://cataas.com/cat')
+        responded=True
+    if not responded:
+        msg.body('Sorry')
+    return str(resp)
